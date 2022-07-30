ae83b70b5e754a6c485d47e43cbd984731b21244
Emilio Manzotti <emilio.manzotti0006@gmail.com>
2022-07-30 12:34:52 +0200

    Add project files.

---
+++ Vector-KMeans_test/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Vector_KMeans_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToVectorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.kMeansToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 48);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToVectorToolStripMenuItem,
+            this.disconnectToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.kMeansToolStripMenuItem,
+            this.saveResultsToolStripMenuItem});
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(100, 40);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // connectToVectorToolStripMenuItem
+            // 
+            this.connectToVectorToolStripMenuItem.Name = "connectToVectorToolStripMenuItem";
+            this.connectToVectorToolStripMenuItem.Size = new System.Drawing.Size(341, 44);
+            this.connectToVectorToolStripMenuItem.Text = "Connect to Vector";
+            this.connectToVectorToolStripMenuItem.Click += new System.EventHandler(this.connectToVectorToolStripMenuItem_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(341, 44);
+            this.disconnectToolStripMenuItem.Text = "Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.disconnectToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(338, 6);
+            // 
+            // kMeansToolStripMenuItem
+            // 
+            this.kMeansToolStripMenuItem.Name = "kMeansToolStripMenuItem";
+            this.kMeansToolStripMenuItem.Size = new System.Drawing.Size(341, 44);
+            this.kMeansToolStripMenuItem.Text = "KMeans";
+            this.kMeansToolStripMenuItem.Click += new System.EventHandler(this.kMeansToolStripMenuItem_Click);
+            // 
+            // saveResultsToolStripMenuItem
+            // 
+            this.saveResultsToolStripMenuItem.Name = "saveResultsToolStripMenuItem";
+            this.saveResultsToolStripMenuItem.Size = new System.Drawing.Size(341, 44);
+            this.saveResultsToolStripMenuItem.Text = "Save Results";
+            this.saveResultsToolStripMenuItem.Click += new System.EventHandler(this.saveResultsToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 402);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToVectorToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kMeansToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem saveResultsToolStripMenuItem;
+    }
+}
+
---
+++ Vector-KMeans_test/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vector;
+using emiLab.Lib.ImageLib;
+using Accord.MachineLearning;
+
+namespace Vector_KMeans_test
+{
+    public partial class Form1 : Form
+    {
+        Robot robot;
+        Bitmap inputBitmap;
+        double[] arrayImage;
+        double[][] data;
+        int contatore, maxSample, NClusters;
+        KMeans kmean;
+        double[][] centroids;
+
+        public Form1()
+        {
+            InitializeComponent();
+            maxSample = 100;
+            robot = new Robot();
+            inputBitmap = new Bitmap(1280, 640);
+            arrayImage = new double[2048];
+            data = new double[maxSample][];
+            contatore = 0;
+            NClusters = 16;
+            centroids = new double[NClusters][];
+        }
+
+        private async void connectToVectorToolStripMenuItem_Click(object sender, EventArgs e)
+        { 
+            await robot.ConnectAsync("A1N3");
+            robot.SuppressPersonalityAsync().ThrowFeedException();
+            await robot.WaitTillPersonalitySuppressedAsync();
+            await robot.Audio.SayTextAsync("all done");
+            robot.Camera.CameraFeedAsync().ThrowFeedException();
+
+            robot.Camera.OnImageReceived += (os, oe) =>
+            {
+                inputBitmap = (Bitmap)oe.Image;
+                ImageProc.Resize(ref inputBitmap, 64, 32);
+                ImageProc.Gray(ref inputBitmap);
+                arrayImage = new double[2048];
+                for (int j = 0; j < inputBitmap.Height; j++)
+                    for (int i = 0; i < inputBitmap.Width; i++)
+                    {
+                        var pixel = inputBitmap.GetPixel(i, j);
+                        arrayImage[j * inputBitmap.Width + i] = (pixel.R + pixel.G + pixel.B) / 3;
+                    }
+
+                if (contatore < maxSample)
+                {
+                    data[contatore] = arrayImage;
+                    contatore++;
+                    Console.WriteLine(contatore);
+                }
+
+                pictureBox1.Invalidate();
+            };
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(inputBitmap, 0, 0, Width, Height);
+        }
+
+        private void saveResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog pippo = new SaveFileDialog();
+            pippo.ShowDialog();
+
+            int size = (int)(Math.Sqrt(NClusters));
+            Bitmap results = new Bitmap(size * inputBitmap.Width, size * inputBitmap.Height);
+
+            int i, j, jj, ii;
+            for (int k = 0; k < centroids.GetLength(0); k++)
+            {
+                j = k / size;
+                i = k % size;
+
+                for (int kk = 0; kk < centroids[k].GetLength(0); kk++)
+                {
+                    jj = kk / 64;
+                    ii = kk % 64;
+                    Color c = Color.FromArgb((int)centroids[k][kk],(int)centroids[k][kk], (int)centroids[k][kk]);
+                    results.SetPixel(ii + i * 64, jj + j * 32, c);
+                }
+            }
+            results.Save(pippo.FileName);
+        }
+
+        private void kMeansToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            kmean = new KMeans(NClusters);
+            KMeansClusterCollection clustersKmeans = kmean.Learn(data);
+            int[] labels = clustersKmeans.Decide(data);
+            centroids = kmean.Centroids;
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (robot.IsConnected)
+            {
+                robot.DisconnectAsync();
+                Application.Exit();
+            }
+        }
+    }
+}
